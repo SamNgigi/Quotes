66a5a543a0d1870e382af8f5dee7ecbb019e5153
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-04 18:32:39 +0300

    Added header component with additional fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Boot4TestComponent } from './boot4-test/boot4-test.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Boot4TestComponent
+    Boot4TestComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
